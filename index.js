beb9c9f7345b0ca5006e8cf28a7fa064e8ae5a30
pharaon3 <pharaon313@outlook.com>
2023-11-28 07:34:36 +0900

    json files

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
 const { DOMParser } = require("xmldom");
 
 let wagerData = [];
+let market_rules = [];
+let soccerEventArray = [];
+let soccerDetail = [];
 let config = {
   method: "get",
   maxBodyLength: Infinity,
@@ -47,9 +52,49 @@ axios
         feedData: feedData,
       };
     });
-
-    console.log("newRows: ", newRows);
+    wagerData = newRows;
+    // console.log("newRows: ", newRows);
   })
   .catch((error) => {
     console.log(error);
   });
+// Get the current directory
+const currentDirectory = process.cwd();
+
+// Specify the file names
+const fileNames = [
+  "market_rules.json",
+  "footballEventData.json",
+  "footballDetail.json",
+];
+
+// Variables to store the data
+let marketRulesData, footballEventData, footballDetailData;
+
+// Read each specified JSON file
+fileNames.forEach((fileName) => {
+  const filePath = path.join(currentDirectory, fileName);
+
+  try {
+    // Read the file synchronously to simplify the example
+    const data = fs.readFileSync(filePath, "utf8");
+
+    // Parse the JSON data and store in the corresponding variable
+    if (fileName === "market_rules.json") {
+      marketRulesData = JSON.parse(data);
+    } else if (fileName === "footballEventData.json") {
+      footballEventData = JSON.parse(data);
+    } else if (fileName === "footballDetail.json") {
+      footballDetailData = JSON.parse(data);
+    }
+  } catch (readError) {
+    console.error(`Error reading or parsing ${fileName}:`, readError);
+  }
+});
+
+setTimeout(() => {
+  console.log("wagerData:", wagerData);
+  console.log("marketRulesData:", marketRulesData);
+  console.log("footballEventData:", footballEventData);
+  console.log("footballDetailData:", footballDetailData);
+}, 2000);
